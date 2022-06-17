5e84c593839e9443e4ed17d8e59586cab7cd1d9a
turbodoubiez <romberg118+github_alt@gmail.com>
2022-06-17 12:09:49 +0200

    Add optional url param to formatter route

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -31,7 +31,7 @@
       </ul>
 
       <div class="-mb-2 text-center md:text-left">
-        <router-link class="nav-link" to="/">Formatter</router-link>
+        <router-link class="nav-link" to="/formatter">Formatter</router-link>
         <router-link class="nav-link" to="/calculator">Calculator</router-link>
       </div>
     </div>
